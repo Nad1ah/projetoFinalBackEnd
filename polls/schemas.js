fe138078d6039f5e50e2c201b748bd1657424e73
Nadiah Mauricio <nadiahmauricio@gmail.com>
2024-03-07 15:50:42 +0000

    alt expiresAt

--- polls/schemas.js
+++ polls/schemas.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const createPollSchema = Joi.object({
   question: Joi.string().required(),
   options: Joi.array().items(Joi.string()).min(2).max(10).required(),
-  expiresAt: Joi.date().max(new Date()),
+  expiresAt: Joi.date().max(new Date().getDate() + 7), // one year
 });
 
 const voteSchema = Joi.object({
